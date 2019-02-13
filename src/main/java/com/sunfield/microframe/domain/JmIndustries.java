f2e8d5d0c4a363359f43befbe5ae7bc213f237e2
Xiaobai0419 <minexiaobai@hotmail.com>
2019-02-13 18:05:30 +0800

    提交行业Bean.

---
+++ src/main/java/com/sunfield/microframe/domain/JmIndustries.java
@@ -0,0 +1,26 @@
+package com.sunfield.microframe.domain;
+
+import com.sunfield.microframe.domain.base.BaseDomain;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * jm_industries bean
+ * @author sunfield coder
+ */
+@ApiModel(value="JmIndustries", description="")
+public class JmIndustries extends BaseDomain{
+
+	@ApiModelProperty(value="行业类别", dataType="String")
+	private String name;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}

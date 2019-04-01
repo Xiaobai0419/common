b37e772c5bec72bbb608ec84a8c6c7f45807b4eb
wangnan <wn198736@163.com>
2019-04-01 13:45:22 +0800

    更新敏感词

--- src/main/java/com/sunfield/microframe/domain/JmSensitiveWord.java
+++ src/main/java/com/sunfield/microframe/domain/JmSensitiveWord.java
@@ -3,6 +3,9 @@ package com.sunfield.microframe.domain;
 import com.sunfield.microframe.domain.base.BaseDomain;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
 
 /**
  * jm_sensitive_word bean
@@ -13,8 +16,9 @@ public class JmSensitiveWord extends BaseDomain{
 
 	@ApiModelProperty(value="敏感词内容", dataType="String")
 	private String content;
-	
-	
+
+	@NotBlank(message="敏感词内容不能为空")
+	@Length(min=0, max=64, message="敏感词内容长度不能超过 64 个字符")
 	public String getContent() {
 		return content;
 	}

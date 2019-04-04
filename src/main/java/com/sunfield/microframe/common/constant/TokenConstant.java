9d419d8c1a830af56427342e8f469e7d88db6d34
wangnan <wn198736@163.com>
2019-04-04 15:18:08 +0800

    更新常量类

---
+++ src/main/java/com/sunfield/microframe/common/constant/TokenConstant.java
@@ -0,0 +1,13 @@
+package com.sunfield.microframe.common.constant;
+
+/**
+ * token相关常量
+ */
+public class TokenConstant {
+
+    /**token在header中规定的key**/
+    public final static String TOKEN_HEADS_KEY = "jm-token";
+
+    /**用户token在redis中存储的key**/
+    public final static String TOKEN_REDIS_MAP_KEY = "tokens";
+}
